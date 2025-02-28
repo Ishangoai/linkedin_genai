7dc035f1e064c22eb6e9fa34ef5b12463cfadf15
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-02-28 15:57:04 +0000

    slack send message setup (#22)
    * slack send message setup
    
    * changed channel to post to

--- src/genai_linkedin/main.py
+++ src/genai_linkedin/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from genai_linkedin.model import generate_post_about, generate_technical_guide_about
+from genai_linkedin.slack_send import send_message
 
 
 app = FastAPI()
@@ -17,3 +18,13 @@ def read_response():
     Option_2 = generate_post_about("the trending tech news this week")
     Option_3 = generate_technical_guide_about("regression analysis in Python")
     return {"Option_1": Option_1, "Option_2": Option_2, "Option_3": Option_3}
+
+
+@app.get("/run_send")
+def send_response():
+    Option_1 = generate_post_about("the latest trends in AI and ML")
+    Option_2 = generate_post_about("the trending tech news this week")
+    Option_3 = generate_technical_guide_about("regression analysis in Python")
+    message = f"Option 1: {Option_1}\n\nOption 2: {Option_2}\n\nOption 3: {Option_3}"
+    res = send_message(message)
+    return "sent" if res else "error"
