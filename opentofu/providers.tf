729cdfabc434b67d9584a772710278d84fcf5b44
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-03-16 13:14:59 +0000

    Updated Opentofu configurations (#24)
    * modified .gitignore to include opentofu and pycharm
    
    * added main.tf opentofu configurations
    
    * added storage bucket to main.tf
    
    * Terraform enabled gcp remote backend for opentofu
    
    * added tofu Github action workflow
    
    * modified tofu Github action workflow
    
    * modified tofu Github action workflow
    
    * fixed setup path error in opentofu
    
    * checkpoint: testing tofu plan build
    
    * checkpoint: testing tofu plan build
    
    * checkpoint: fixed gcloud auth - removed impersonate command
    
    * checkpoint: fixed error - cd opentofu
    
    * checkpoint: fixed error - cd opentofu
    
    * checkpoint: test fixed error - cd opentofu
    
    * checkpoint: fixed error - working directory
    
    * checkpoint: fixed error - tofu fmt
    
    * checkpoint: fixed error - set trace logs - true
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * lkl
    
    * .
    
    * .
    
    * .
    
    * try drop-in replacement with op5dev/tf-via-pr
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * use tofu instead of terraform
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * update secrets input
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * update permissions
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * merge `plan` and `apply` steps together
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * add `arg-lock` options
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * move resources in named files
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * test `command: apply` within PR context
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * amend dynamic environment protection
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    
    * Update opentofu-provision.yml
    
    ---------
    
    Signed-off-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>
    Co-authored-by: Oliver Angélil <olivermangelil@gmail.com>
    Co-authored-by: Rishav Dhar <19497993+rdhar@users.noreply.github.com>

--- opentofu/providers.tf
+++ opentofu/providers.tf
@@ -1,1 +1,12 @@
+terraform {
+  backend "gcs" {
+    bucket = "linkedin_genai_bucket"
+    prefix = "terraform/state"
+  }
+}
 
+# Provider configuration (optional, if not using Application Default Credentials)
+# provider "google" {
+#   project     = "linkedin-genai"
+#   credentials = file("credentials.json")  # Uncomment if using a service account key
+# }
---
+++ opentofu/variables.tf
@@ -0,0 +1,22 @@
+# Variable definitions for sensitive data.
+variable "gemini_api_key" {
+  description = "API key for Gemini"
+  type        = string
+  sensitive   = true
+}
+
+variable "slack_api_token" {
+  description = "API token for Slack"
+  type        = string
+  sensitive   = true
+}
+
+variable "billing_account" {
+  description = "Billing account ID"
+  type        = string
+}
+
+variable "org_id" {
+  description = "Organization ID"
+  type        = string
+}
