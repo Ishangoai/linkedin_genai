55c01e1a46068a49cd546da4caa5f60155912a22
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-03-28 08:24:47 +0000

    Feature branch vincent (#35)
    * fix for delte resource blocked
    
    * fixed fmt error
    
    * fixed reference error
    
    * switched to use project_number variablein main.tf
    
    * setting up to deploy only after tofu is provisioned
    
    * fix fmt error

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -13,7 +13,7 @@ resource "google_artifact_registry_repository_iam_member" "workload_identity_bin
   location   = google_artifact_registry_repository.artifact_registry_repository.location
   repository = google_artifact_registry_repository.artifact_registry_repository.repository_id
   role       = "roles/artifactregistry.writer"
-  member     = "principalSet://iam.googleapis.com/projects/257550582362/locations/global/workloadIdentityPools/github/attribute.event_name/push"
+  member     = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github/attribute.event_name/push"
 }
 
 resource "google_service_account" "cloud_run_service_account" {
@@ -23,14 +23,13 @@ resource "google_service_account" "cloud_run_service_account" {
 }
 
 resource "google_cloud_run_v2_service" "cloud_run_service" {
-  client              = "gcloud"
-  client_version      = "511.0.0"
-  ingress             = "INGRESS_TRAFFIC_ALL"
-  launch_stage        = "GA"
-  location            = "europe-west2"
-  name                = "${var.gcp_project_name}-service"
-  project             = var.gcp_project_name
-  deletion_protection = false
+  client         = "gcloud"
+  client_version = "511.0.0"
+  ingress        = "INGRESS_TRAFFIC_ALL"
+  launch_stage   = "GA"
+  location       = "europe-west2"
+  name           = "${var.gcp_project_name}-service"
+  project        = var.gcp_project_name
   template {
     containers {
       env {
