ed842f44015aabaf3cde6591cf61b9c6f097d331
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-04-04 09:13:20 +0000

    fixed timeout when AI processing

--- src/genai_linkedin/main.py
+++ src/genai_linkedin/main.py
@@ -1,7 +1,8 @@
+import asyncio
 from fastapi import FastAPI, Request, HTTPException, Response
 from slack_sdk import WebhookClient
 
-from genai_linkedin.model import generate_post_about, generate_technical_guide_about
+from genai_linkedin.model import generate_post_about, generate_technical_guide_about, process_run_send
 from genai_linkedin.slack_send import send_message, is_valid_signature
 
 
@@ -33,7 +34,6 @@ def send_response():
 
 @app.post("/slack/events")
 async def webhook(request: Request):
-
     if not await is_valid_signature(request):
         # if slack challenge request
         if request.method == "GET":
@@ -42,8 +42,7 @@ async def webhook(request: Request):
         raise HTTPException(status_code=403, detail="Invalid signature")
 
     form = await request.form()
-    if "command" in form \
-        and form["command"] == "/run_test":
+    if "command" in form and form["command"] == "/run_test":
         response_url = str(form["response_url"])
         text = form["text"]
         webhook = WebhookClient(response_url)
@@ -53,20 +52,22 @@ async def webhook(request: Request):
         print(response.status_code)
         return Response("", 200)
 
-    if "command" in form \
-        and form["command"] == "/run_send":
+    if "command" in form and form["command"] == "/run_send":
         response_url = str(form["response_url"])
-        Option_1 = generate_post_about("the latest trends in AI and ML")
-        Option_2 = generate_post_about("the trending tech news this week")
-        Option_3 = generate_technical_guide_about("regression analysis in Python")
-        message = f"Option 1: {Option_1}\n\nOption 2: {Option_2}\n\nOption 3: {Option_3}"
-        webhook = WebhookClient(response_url)
-        # Send a reply in the channel
-        response = webhook.send(text=message)
-        # Acknowledge this request
-        print(response.status_code)
+        # Schedule the async task
+        asyncio.create_task(send_generated_content(response_url))
+        # Immediately acknowledge the request
         return Response("", 200)
 
     else:
         # Handle other events
         return Response("success", 200)
+
+
+async def send_generated_content(response_url: str):
+    # Generate the content
+    message = process_run_send()
+    # Send the message asynchronously
+    webhook = WebhookClient(response_url)
+    response = webhook.send(text=message)
+    print(response.status_code)
